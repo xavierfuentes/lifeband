494028a078f5955ba06866c77391d17cb6d91875
Xavi Fuentes Fernández <xavifuefer@Xavis-MacBookPro.local>
2014-06-28 17:29:13 +0100

    App skeleton

---
+++ src/app/about/about.js
@@ -0,0 +1,24 @@
+angular.module( 'lifeBand.about', [
+  'ui.router',
+  'placeholders',
+  'ui.bootstrap'
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'about', {
+    url: '/about',
+    views: {
+      "main": {
+        controller: 'AboutCtrl',
+        templateUrl: 'about/about.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'About' }
+  });
+})
+
+.controller( 'AboutCtrl', function AboutCtrl( $scope ) {
+
+})
+
+;
---
+++ src/app/app.js
@@ -0,0 +1,24 @@
+angular.module( 'lifeBand', [
+  'templates-app',
+  'templates-common',
+  'lifeBand.home',
+  'lifeBand.about',
+  'ui.router'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | lifeBand' ;
+    }
+  });
+})
+
+;
